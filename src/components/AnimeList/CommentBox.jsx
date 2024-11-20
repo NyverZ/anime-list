a219cafeda976bdb24dde1bc767905e96146de82
NyverZ <161116470+NyverZ@users.noreply.github.com>
2024-11-20 09:10:31 +0800

    Update CommentBox.jsx

--- src/components/AnimeList/CommentBox.jsx
+++ src/components/AnimeList/CommentBox.jsx
@@ -1,12 +1,11 @@
 import prisma from '@/libs/prisma'
 import React from 'react'
 
-const CommentBox = async ({ anime_mal_id }) => {
-    // const comments = await prisma.comment.findMany({ where: { anime_mal_id } });
+const CommentBox = () => {
 
     return (
         <div className='grid grid-cols-4 gap-4 mb-4'>
-            Comments 
+            ...
         </div>
     )
 }
